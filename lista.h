a2d1b441149a4d9c58339c722ef606eca6539308
Nickson8 <nicolasdesousamaia@gmail.com>
2024-09-15 17:14:02 -0300

    Doc 1.0 lista.h

--- lista.h
+++ lista.h
@@ -3,14 +3,22 @@
 
     typedef struct lista LISTA;
 
+    // Tamanho máximo da lista
     #define TAM_MAX 20
 
+    // Criar a lista, retornando o ponteiro para ela
     LISTA* lista_criar();
+    // Adiciona um elemento (x) ao fim da lista
     void lista_adicionar_fim(LISTA* lista, int x);
+    // Remove o último item da lista
     void lista_remover_fim(LISTA* lista);
+    // Retorna o elemento a partir de seu "indice" na lista
     int lista_buscar(LISTA* lista, int indice);
+    // Retorna o tamanho da lista
     int lista_tamanho(LISTA* lista);
+    //Limpa a lista de seus elementos
     void lista_limpar(LISTA* lista);
+    //Apaga a lista
     void lista_apagar(LISTA** lista);
 
 #endif
